4d578ca189ffbf17c719d4f7ea75e97ec88cbcaf
LESTER HERNANDEZ CRUZ <lesterherdez03@unitec.edu>
2024-09-17 19:47:05 -0600

    Commit 3

---
+++ RegistrationSystem.ReportService/IRegistrationService.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections.Generic;
+using System.Threading.Tasks;
+
+public interface IRegistrationService
+{
+    Task<IEnumerable<Registration>> GetSuccessfulRegistrationsAsync();
+}
+
+public class Registration
+{
+    public string StudentName { get; set; }
+    public string AccountNumber { get; set; }
+    public List<string> Courses { get; set; }
+}
--- RegistrationSystem.ReportService/Worker.cs
+++ RegistrationSystem.ReportService/Worker.cs
@@ -3,10 +3,12 @@ namespace RegistrationSystem.ReportService
     public class Worker : BackgroundService
     {
         private readonly ILogger<Worker> _logger;
+        private readonly IRegistrationService _registrationService;
 
-        public Worker(ILogger<Worker> logger)
+        public Worker(ILogger<Worker> logger, IRegistrationService registrationService)
         {
             _logger = logger;
+            _registrationService = registrationService;
         }
 
         protected override async Task ExecuteAsync(CancellationToken stoppingToken)
@@ -15,7 +17,16 @@ namespace RegistrationSystem.ReportService
             {
                 if (_logger.IsEnabled(LogLevel.Information))
                 {
-                    _logger.LogInformation("Worker running at: {time}", DateTimeOffset.Now);
+                    var registrations = await _registrationService.GetSuccessfulRegistrationsAsync();
+                    foreach (var registration in registrations)
+                    {
+                        var message = $"Matrícula exitosa para el estudiante {registration.StudentName} con cuenta {registration.AccountNumber} en los cursos:\n\n";
+                        foreach (var course in registration.Courses)
+                        {
+                            message += $"{course}\n\n";
+                        }
+                        _logger.LogInformation(message);
+                    }
                 }
                 await Task.Delay(1000, stoppingToken);
             }
